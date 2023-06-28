aa4f765e0b73e01687cba9bc22334474a6d0c3a2
SHANMUGAM R <shanmugamrskfamily@gmail.com>
2023-06-28 18:28:36 +0530

    Program updated

--- DAY 04 TASK/01_Anonymous Function and IIFE/a_Print_Odd_Numbers.js
+++ DAY 04 TASK/01_Anonymous Function and IIFE/a_Print_Odd_Numbers.js
@@ -1,9 +1,10 @@
 (function(arr) {
-    arr.forEach(function(num) 
+  for (var i = 0; i < arr.length; i++) 
+  {
+    var num = arr[i];
+    if (num % 2 !== 0) 
     {
-      if (num % 2 !== 0) 
-      {
-        console.log(num);
-      }
-    });
-  })([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      console.log(num);
+    }
+  }
+})([1, 2, 3, 4, 5, 6, 7, 8, 9]);
--- DAY 04 TASK/02_Arrow_Functions/a_Print_Odd_Numbers.js
+++ DAY 04 TASK/02_Arrow_Functions/a_Print_Odd_Numbers.js
@@ -1,9 +1,10 @@
 ((arr) => {
-    arr.forEach((num) => 
+  for (let i = 0; i < arr.length; i++) 
+  {
+    const num = arr[i];
+    if (num % 2 !== 0) 
     {
-      if (num % 2 !== 0) 
-      {
-        console.log(num);
-      }
-    });
-  })([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      console.log(num);
+    }
+  }
+})([1, 2, 3, 4, 5, 6, 7, 8, 9]);
